b665f3378aeb20f78bc8a30b05b18b2eb6c59b34
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 21:59:14 -0600

    added get post by id

--- router/postsRouter.js
+++ router/postsRouter.js
@@ -12,6 +12,19 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'There was an error retrieving the posts'})
     }
 })
+//GET post by id
+router.get('/:id', async (req, res) => {
+    try{
+        const post = await Posts.getById(req.params.id);
+        if (post){
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID could not be retrieved'});
+        }
+    } catch {
+        res.status(500).json({ message: 'The post information could not be retrieved'})
+    }
+})
 
 module.exports = router;
 
