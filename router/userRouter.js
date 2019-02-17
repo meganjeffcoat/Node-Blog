39939bcabb174af2b0aee30652289b25fde56cd3
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 21:22:58 -0600

    ability to get user by id implimented

--- router/userRouter.js
+++ router/userRouter.js
@@ -13,4 +13,17 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
+        if (user){
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'The user with the specified ID could not be found'});
+        }
+    } catch {
+        res.status(500).json({ message: 'The user information could not be retrieved'})
+    }
+})
+
 module.exports = router;
