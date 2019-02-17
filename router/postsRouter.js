3f9c03bd9c55ec67ea89d403e7803b522db6464a
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 21:18:31 -0600

    fixed server issue and now initial get is rendering

--- router/postsRouter.js
+++ router/postsRouter.js
@@ -1,4 +1,17 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// const Posts = require('../data/helpers/postDb');
+const Posts = require('../data/helpers/postDb');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get();
+        res.status(200).json(posts);
+    } catch {
+        res.status(500).json({ error: 'There was an error retrieving the posts'})
+    }
+})
+
+module.exports = router;
+
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,3 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const Users = require('../data/helpers/userDb');
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get();
+        res.status(200).json(users);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error retrieving users'})
+    }
+})
+
+module.exports = router;
